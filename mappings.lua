7a271a33693e7df517dfc6e8761522ed383c6dc4
Rafael Thayto <rafa.thayto@gmail.com>
2023-04-13 18:01:46 -0300

    feat: initial configs

---
+++ mappings.lua
@@ -0,0 +1,42 @@
+-- Mapping data with "desc" stored directly by vim.keymap.set().
+--
+-- Please use this mappings table to set keyboard mapping since this is the
+-- lower level configuration and more robust one. (which-key will
+-- automatically pick-up stored data by this setting.)
+return {
+  -- first key is the mode
+  n = {
+    -- second key is the lefthand side of the map
+    -- mappings seen under group name "Buffer"
+    ["<leader>bn"] = { "<cmd>tabnew<cr>", desc = "New tab" },
+    ["<leader>bD"] = {
+      function()
+        require("astronvim.utils.status").heirline.buffer_picker(
+          function(bufnr) require("astronvim.utils.buffer").close(bufnr) end
+        )
+      end,
+      desc = "Pick to close",
+    },
+    -- tables with the `name` key will be registered with which-key if it's installed
+    -- this is useful for naming menus
+    ["<leader>b"] = { name = "Buffers" },
+    -- quick save
+    --
+    -- ["<C-s>"] = { ":w!<cr>", desc = "Save File" },  -- change description but the same command
+    ["<leader>ss"] = { ":%s/", desc = "Search and Replace RegExp" },
+    ["<C-p>"] = { function() require("telescope.builtin").find_files() end, desc = "Find files" },
+    ["<C-d>"] = { "<C-d>zz" },
+    ["<C-u>"] = { "<C-u>zz" },
+    ["n"] = { "nzzzv" },
+    ["N"] = { "Nzzzv" },
+    -- ["<leader>gg"] = { ":G<CR>", desc = "Fugitive" },
+  },
+  t = {
+    -- setting a mapping to false will disable it
+    -- ["<esc>"] = false,
+  },
+  v = {
+    ["J"] = { ":m '>+1<cr>gv=gv" },
+    ["K"] = { ":m '<-2<cr>gv=gv" },
+  },
+}
---
+++ plugins/user.lua
@@ -0,0 +1,17 @@
+return {
+  -- You can also add new plugins here as well:
+  -- Add plugins, the lazy syntax
+  -- "andweeb/presence.nvim",
+  -- {
+  --   "ray-x/lsp_signature.nvim",
+  --   event = "BufRead",
+  --   config = function()
+  --     require("lsp_signature").setup()
+  --   end,
+  -- },
+  {
+    "catppuccin/nvim",
+    as = "catppuccin",
+    config = function() require("catppuccin").setup {} end,
+  },
+}
