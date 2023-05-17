e85f38e7d2d900d25fb1f8e55abd7747a7657c4c
Rafael Thayto <rafa.thayto@gmail.com>
2023-05-17 12:01:32 -0300

    feat: adds nvim-treesitter-context plugin on treesitter

--- plugins/treesitter.lua
+++ plugins/treesitter.lua
@@ -1,5 +1,8 @@
 return {
   "nvim-treesitter/nvim-treesitter",
+  dependencies = {
+    { "nvim-treesitter/nvim-treesitter-context", opts = { enable = true } },
+  },
   opts = {
     -- ensure_installed = { "lua" },
   },
