e74dbf340fe0d28c02070dc401feaebb3f64a034
Rafael Thayto <rafa.thayto@gmail.com>
2023-05-18 14:05:18 -0300

    feat: adds markdown-preview plugin

--- plugins/user.lua
+++ plugins/user.lua
@@ -10,6 +10,11 @@ return {
   --   end,
   -- },
   {
+    "iamcco/markdown-preview.nvim",
+    ft = "markdown",
+    build = function() vim.fn["mkdp#util#install"]() end,
+  },
+  {
     "tpope/vim-surround",
   },
   {
