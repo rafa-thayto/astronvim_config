c2040a8c4f8df6d474a3dd344e0425c4320caa89
Rafael Thayto <rafa.thayto@gmail.com>
2023-05-20 21:48:01 -0300

    feat: adds VimBeGood plugin

--- plugins/user.lua
+++ plugins/user.lua
@@ -15,6 +15,15 @@ return {
     build = function() vim.fn["mkdp#util#install"]() end,
   },
   {
+    'styled-components/vim-styled-components',
+  },
+  {
+    'ThePrimeagen/vim-be-good',
+    cmd = {
+      "VimBeGood",
+    }
+  },
+  {
     "tpope/vim-surround",
   },
   {
